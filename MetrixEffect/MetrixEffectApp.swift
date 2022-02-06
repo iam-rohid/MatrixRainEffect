c1790e256be8e96f0c8cd2d04e8d8d3e54d4dd85
rohid-hub <rohidul209@gmail.com>
2022-02-06 18:17:26 +0600

    Initial Commit

---
+++ MetrixEffect/MetrixEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetrixEffectApp.swift
+//  MetrixEffect
+//
+//  Created by Rohidul Islam on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MetrixEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
